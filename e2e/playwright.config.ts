bfe9c5a417f0f7cd3618a63b7ac899a49fcfd2b8
Brian <sthitpanth01@gmail.com>
2026-05-09 20:03:16 +0200

    add time limits and backend check

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,9 +5,10 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: 1,
-  reporter: process.env.CI ? 'github' : 'html',
+  timeout: 15000,
+  reporter: process.env.CI ? [['github'], ['list']] : [['html']],
 
   use: {
     baseURL: 'http://localhost:80',
